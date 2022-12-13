8e4dff82ad074b33f9504e97325e8ab314cfae2c
ecanvasser <eddiecanvassere9@gmail.com>
2022-12-13 03:50:41 -0800

    Add subtraction tests

--- exercises.js
+++ exercises.js
@@ -1,14 +1,42 @@
 const capitalize = (s) => {
-    let arr = s.split('');
-    arr[0] = arr[0].toUpperCase();
-    return arr.toString().replaceAll(',', '')
- }
+  let arr = s.split("");
+  arr[0] = arr[0].toUpperCase();
+  return arr.toString().replaceAll(",", "");
+};
 
- const reverseString = (s) => {
-    if (s.length === 0) {
-        return s
+const reverseString = (s) => {
+  if (s.length === 0) {
+    return s;
+  }
+  return reverseString(s.substr(1)) + s.charAt(0);
+};
+
+const calculator = () => {
+  const add = (a, b) => {
+    return a + b;
+  };
+
+  const subtract = (a, b) => {
+    if (b < 0) {
+      return a + Math.abs(b);
     }
-    return reverseString(s.substr(1)) + s.charAt(0)
- }
+    return a - b;
+  };
 
-export {capitalize, reverseString}    
+  const multiply = (a, b) => {
+    return a * b;
+  };
+
+  const divide = (a, b) => {
+    return a / b;
+  };
+
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+  };
+};
+
+export { capitalize, reverseString, calculator };
--- test.js
+++ test.js
@@ -1,9 +1,27 @@
-import { capitalize, reverseString } from './exercises'
+import { capitalize, reverseString, calculator } from "./exercises";
 
-test('String gets capitalized', () => {
-    expect(capitalize('app')).toBe('App');
-})
+test("String gets capitalized", () => {
+  expect(capitalize("app")).toBe("App");
+});
 
-test('String gets reversed', () => {
-    expect(reverseString('Test')).toBe('tseT');
-})
+test("String gets reversed", () => {
+  expect(reverseString("Test")).toBe("tseT");
+});
+
+describe("Calculator object methods", () => {
+  test("Addition", () => {
+    expect(calculator().add(1, 2)).toBe(3);
+  });
+
+  test("Subtract", () => {
+    expect(calculator().subtract(-5, 3)).toBe(-8);
+  });
+
+  test("Subtract negative b", () => {
+    expect(calculator().subtract(5, -3)).toBe(8);
+  });
+  test("Subtract two negatives", () => {
+    expect(calculator().subtract(-5, -3)).toBe(-2);
+  });
+  
+});
